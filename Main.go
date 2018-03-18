c777521e23ae2a28ccb413f7d167312b2a4985fa
NekitMalyarenko <n.a.m.62608@gmail.com>
2018-03-18 18:30:49 +0200

    Add files via upload

---
+++ Main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/NekitMalyarenko/VocabularyBot/db"
+	"github.com/NekitMalyarenko/VocabularyBot/telegram"
+)
+
+func main() {
+	db.GetDBManager()
+	defer db.CloseConnection()
+
+	telegram.Start()
+	fdg
+}
